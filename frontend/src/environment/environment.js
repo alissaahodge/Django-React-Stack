91bb6fc7ef8f4dafd8a92e0274186f6bd269c974
Alissa <alissaahodge@gmail.com>
2021-07-28 14:10:29 -0400

    Removed environment frontend file

--- frontend/src/environment/environment.js
+++
@@ -1,5 +0,0 @@
-export const BACKEND_API = "https://localhost:8000/api/v1";
-export const GOOGLE_CLIENT_ID = "kjsdkjsdbjksabdjksabd";
-export const GRANT_TYPE = "password";
-export const CLIENT_ID = "57f35283-0dae-4618-9012-cb73ecf5ad99";
-export const CLIENT_SECRET = "abc123";
